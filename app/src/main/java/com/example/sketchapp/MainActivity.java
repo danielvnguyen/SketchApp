d45a04578801e449b5a7fd01dcd226dcf0045a30
Daniel Nguyen <danielnguyen604@gmail.com>
2021-12-18 21:30:15 -0800

    initial commit

---
+++ app/src/main/java/com/example/sketchapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sketchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
