1c13f03b7b2af04ba1078cbd6ff2f5d50b6a8c23
Daniel Nguyen <danielnguyen604@gmail.com>
2021-12-20 17:07:28 -0800

    create welcome screen

--- app/src/main/java/com/example/sketchapp/UI/MainMenu.java
+++ app/src/main/java/com/example/sketchapp/UI/MainMenu.java
@@ -2,6 +2,8 @@ package com.example.sketchapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.sketchapp.R;
@@ -12,5 +14,10 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        setTitle("Main Menu");
+    }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, MainMenu.class);
     }
 }
--- app/src/main/java/com/example/sketchapp/UI/WelcomeScreen.java
+++ app/src/main/java/com/example/sketchapp/UI/WelcomeScreen.java
@@ -2,14 +2,38 @@ package com.example.sketchapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 
 import com.example.sketchapp.R;
 
 public class WelcomeScreen extends AppCompatActivity {
 
+    private Boolean screenEnded = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
+        if (getSupportActionBar() != null) getSupportActionBar().hide();
+
+        fadeIn(findViewById(R.id.welcome_screen_title));
+        fadeIn(findViewById(R.id.welcome_screen_author));
+        Handler loadingHandler = new Handler();
+        loadingHandler.postDelayed(this::startMainMenu, 4000);
+    }
+
+    private void startMainMenu() {
+        if (!screenEnded) {
+            screenEnded = true;
+            startActivity(MainMenu.makeIntent(this));
+            finish();
+        }
+    }
+
+    private void fadeIn(View view) {
+        view.setAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        view.animate();
     }
 }
