7846e8b63a228923831bd639983a0c40433e2bc9
Daniel Nguyen <danielnguyen604@gmail.com>
2021-12-20 22:42:00 -0800

    implement functionality for drawing + touch events

--- app/src/main/java/com/example/sketchapp/Model/DrawView.java
+++ app/src/main/java/com/example/sketchapp/Model/DrawView.java
@@ -1,5 +1,6 @@
 package com.example.sketchapp.Model;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -7,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import java.util.ArrayList;
 
@@ -55,6 +57,71 @@ public class DrawView extends View{
         currentColour = Color.GREEN;
         strokeWidth = 20;
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+
+        int backgroundColour = Color.WHITE;
+        mCanvas.drawColor(backgroundColour);
+
+        for (Stroke currentPath: paths) {
+            mPaint.setColor(currentPath.colour);
+            mPaint.setStrokeWidth(currentPath.strokeWidth);
+            mCanvas.drawPath(currentPath.path, mPaint);
+        }
+        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+        canvas.restore();
+    }
+
+    private void touchStart(float x, float y) {
+        mPath = new Path();
+        Stroke currentPath = new Stroke(currentColour, strokeWidth, mPath);
+        paths.add(currentPath);
+
+        mPath.reset();
+        mPath.moveTo(x,y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touchUp() {
+        mPath.lineTo(mX, mY);
+    }
+
+    private void touchMove(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            touchStart(x, y);
+            invalidate();
+        }
+        else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            touchMove(x, y);
+            invalidate();
+        }
+        else if (event.getAction() == MotionEvent.ACTION_UP) {
+            touchUp();
+            invalidate();
+        }
+        return true;
+    }
+
+    //Below is the functionality for the SketchScreen buttons
 
     public void setColour(int colour) {
         currentColour = colour;
